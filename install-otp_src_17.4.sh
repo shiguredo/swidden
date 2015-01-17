96b01e3f0ba9a75dad8ed5509ef4276bf254a37d
Nakai Ryosuke <nakai@shiguredo.jp>
2015-01-17 09:49:24 +0900

    17.4 にあげる

--- install-otp_src_17.4.sh
+++ install-otp_src_17.4.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
-if [ ! -e ~/otp-17.3/bin/erl ]; then
-  curl -O http://www.erlang.org/download/otp_src_17.3.tar.gz
-  tar xzf otp_src_17.3.tar.gz
-  cd otp_src_17.3
-  ./configure --prefix=/home/ubuntu/otp-17.3 \
+if [ ! -e ~/otp-17.4/bin/erl ]; then
+  curl -O http://www.erlang.org/download/otp_src_17.4.tar.gz
+  tar xzf otp_src_17.4.tar.gz
+  cd otp_src_17.4
+  ./configure --prefix=/home/ubuntu/otp-17.4 \
               --enable-smp-support \
               --enable-m64-build \
               --enable-threads \
