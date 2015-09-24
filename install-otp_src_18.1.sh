5c9d6d22b3d5d42478b1b88c83ad4538fa1be8ab
NAKAI Ryosuke <nakai@shiguredo.jp>
2015-09-24 12:22:15 +0900

    18.1 に対応する

--- install-otp_src_18.1.sh
+++ install-otp_src_18.1.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
-if [ ! -e ~/otp-18.0/bin/erl ]; then
-  curl -O http://www.erlang.org/download/otp_src_18.0.tar.gz
-  tar xzf otp_src_18.0.tar.gz
-  cd otp_src_18.0
-  ./configure --prefix=/home/ubuntu/otp-18.0 \
+if [ ! -e ~/otp-18.1/bin/erl ]; then
+  curl -O http://www.erlang.org/download/otp_src_18.1.tar.gz
+  tar xzf otp_src_18.1.tar.gz
+  cd otp_src_18.1
+  ./configure --prefix=/home/ubuntu/otp-18.1 \
               --enable-smp-support \
               --enable-m64-build \
               --enable-threads \
