7ff68163ba7e9d382f47bc3186bb409241d9d8d1
NAKAI Ryosuke <nakai@shiguredo.jp>
2016-02-04 14:12:18 +0900

    18.2.1 に切り替える

--- install-otp_src_18.2.1.sh
+++ install-otp_src_18.2.1.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
-if [ ! -e ~/otp-18.2/bin/erl ]; then
-  curl -O http://www.erlang.org/download/otp_src_18.2.tar.gz
-  tar xzf otp_src_18.2.tar.gz
-  cd otp_src_18.2
-  ./configure --prefix=/home/ubuntu/otp-18.2 \
+if [ ! -e ~/otp-18.2.1/bin/erl ]; then
+  curl -O http://erlang.org/download/otp_src_18.2.1.tar.gz
+  tar xzf otp_src_18.2.1.tar.gz
+  cd otp_src_18.2.1
+  ./configure --prefix=/home/ubuntu/otp-18.2.1 \
               --enable-smp-support \
               --enable-m64-build \
               --enable-threads \
