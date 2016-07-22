954d7ab6ffc8701b6422eb616eb185f153d3d2a0
NAKAI Ryosuke <nakai@shiguredo.jp>
2016-07-22 22:51:12 +0900

    hakckney の close 処理とプールを使わないようにする

--- src/swidden_client.erl
+++ src/swidden_client.erl
@@ -21,16 +21,19 @@ request0(Port, Target, Service, Version, Operation, RawJSON) when is_integer(Por
 request0(RawPort, Target, Service, Version, Operation, RawJSON) ->
     URL = <<"http://127.0.0.1", $:, RawPort/binary, $/>>,
     Headers = [{Target, list_to_binary([Service, $_, Version, $., Operation])}],
-    Options = [],
+    Options = [{pool, false}],
     case hackney:post(URL, Headers, RawJSON, Options) of
         {ok, StatusCode, _RespHeaders, ClientRef} when StatusCode =:= 200 orelse StatusCode =:= 400 ->
             case hackney:body(ClientRef) of
                 {ok, <<>>} ->
+                    hackne:close(ClientRef),
                     {ok, StatusCode};
                 {ok, Body} ->
+                    hackne:close(ClientRef),
                     {ok, StatusCode, jsone:decode(Body)}
             end;
-        {ok, StatusCode, _RespHeaders, _ClientRef} ->
+        {ok, StatusCode, _RespHeaders, ClientRef} ->
+            hackne:close(ClientRef),
             {error, {status_code, StatusCode}};
         {error, Reason} ->
             {error, Reason}
