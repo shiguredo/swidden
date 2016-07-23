87a4f891f9d0384365035fc1089990562610d96c
NAKAI Ryosuke <nakai@shiguredo.jp>
2016-07-23 13:46:37 +0900

    headers を追加できるようにする

--- src/swidden_client.erl
+++ src/swidden_client.erl
@@ -21,14 +21,15 @@ request_with_headers(Port, Headers, Target, Service, Version, Operation) ->
     request0(Port, Headers, Target, Service, Version, Operation, []).
 
 request_with_headers(Port, Headers, Target, Service, Version, Operation, Json) ->
-    request0(Port, Headers, Target, Service, Version, Operation, Json).
+    RawJson = jsone:encode(Json),
+    request0(Port, Headers, Target, Service, Version, Operation, RawJson).
 
 
 request0(Port, Headers, Target, Service, Version, Operation, RawJson) when is_integer(Port) ->
     request0(integer_to_binary(Port), Headers, Target, Service, Version, Operation, RawJson);
-request0(RawPort, Headers, Target, Service, Version, Operation, RawJson) ->
+request0(RawPort, Headers0, Target, Service, Version, Operation, RawJson) ->
     URL = <<"http://127.0.0.1", $:, RawPort/binary, $/>>,
-    Headers = [{Target, list_to_binary([Service, $_, Version, $., Operation])}|Headers],
+    Headers = [{Target, list_to_binary([Service, $_, Version, $., Operation])}|Headers0],
     Options = [{pool, false}],
     case hackney:post(URL, Headers, RawJson, Options) of
         {ok, StatusCode, _RespHeaders, ClientRef} when StatusCode =:= 200 orelse StatusCode =:= 400 ->
