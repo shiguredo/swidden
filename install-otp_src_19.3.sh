31faaed3fd810af98ee877e3ae861ac128747741
NAKAI Ryosuke <nakai@shiguredo.jp>
2017-03-16 00:55:16 +0900

    19.3 に上げる

--- install-otp_src_19.3.sh
+++ install-otp_src_19.3.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
-if [ ! -e ~/otp-19.2/bin/erl ]; then
-  curl -O http://erlang.shiguredo.jp/otp_src_19.2.tar.gz
-  tar xzf otp_src_19.2.tar.gz
-  cd otp_src_19.2
-  ./configure --prefix=/home/ubuntu/otp-19.2 \
+if [ ! -e ~/otp-19.3/bin/erl ]; then
+  curl -O http://erlang.shiguredo.jp/otp_src_19.3.tar.gz
+  tar xzf otp_src_19.3.tar.gz
+  cd otp_src_19.3
+  ./configure --prefix=/home/ubuntu/otp-19.3 \
               --enable-smp-support \
               --enable-m64-build \
               --enable-threads \
