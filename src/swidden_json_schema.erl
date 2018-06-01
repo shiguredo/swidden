2c2dcd9fa38fd069b50c70919a21aa3c4e9af6ab
NAKAI Ryosuke <nakai@shiguredo.jp>
2018-06-01 10:25:24 +0900

    OTP 21.0-rc2 でビルドできるようライブラリの指定を変更する

--- src/swidden_json_schema.erl
+++ src/swidden_json_schema.erl
@@ -22,7 +22,7 @@ start(Name) ->
     end.
 
 
--spec validate_json(binary(), binary(), binary(), binary()) -> ok | {error, term()}.
+-spec validate_json(binary(), binary(), binary(), binary()) -> {ok, module(), function(), jsone:jsonterm()} | {error, term()}.
 validate_json(Service, Version, Operation, RawJSON) ->
     Key = binary_to_list(list_to_binary([Service, $_, Version, $., Operation])),
     case validate(Key, RawJSON) of
