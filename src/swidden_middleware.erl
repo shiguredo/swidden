959eae8322ace3e7fdeaad97d0913d1bc4a59177
voluntas <nakai@shiguredo.jp>
2026-05-28 13:16:01 +0900

    定数化

--- src/swidden_middleware.erl
+++ src/swidden_middleware.erl
@@ -2,16 +2,19 @@
 
 -export([failure/2, failure/3]).
 
+-define(HTTP_HEADER_CONTENT_TYPE,      <<"content-type">>).
+-define(CONTENT_TYPE_APPLICATION_JSON, <<"application/json">>).
+
 
 failure(Req, Type) when is_binary(Type) ->
     cowboy_req:reply(400,
-                     #{<<"content-type">> => <<"application/json">>},
+                     #{?HTTP_HEADER_CONTENT_TYPE => ?CONTENT_TYPE_APPLICATION_JSON},
                      jsone:encode(#{error_type => Type}),
                      Req).
 
 
 failure(Req, Type, Reason) when is_binary(Type) andalso is_map(Reason) ->
     cowboy_req:reply(400,
-                     #{<<"content-type">> => <<"application/json">>},
+                     #{?HTTP_HEADER_CONTENT_TYPE => ?CONTENT_TYPE_APPLICATION_JSON},
                      jsone:encode(#{error_type => Type, error_reason => Reason}),
                      Req).
