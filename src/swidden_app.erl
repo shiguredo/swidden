159c5f4ba6edc834351754548c91487e8252df47
voluntas <nakai@shiguredo.jp>
2026-05-28 13:15:54 +0900

    不要なコメントを削除

--- src/swidden_app.erl
+++ src/swidden_app.erl
@@ -2,12 +2,7 @@
 
 -behaviour(application).
 
-%% Application callbacks
 -export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
 
 
 start(_StartType, _StartArgs) ->
